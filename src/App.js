a37f8257da26d12c4fc66767ebee2e0cfc677d73
Vinicius Lemos <vinycius1993@gmail.com>
2023-03-21 09:23:45 -0300

    feat: adicionado diretório para os componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Formulario from './components/Formulario'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Formulario />
     </div>
   );
 }
---
+++ src/components/Formulario/index.js
@@ -0,0 +1,11 @@
+import './Formulario.css'
+
+function index() {
+  return (
+    <div>
+        <h1>TesTe Componente</h1>
+    </div>
+  )
+}
+
+export default index
